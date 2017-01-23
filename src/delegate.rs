f74219b3e87851dd7b199918fe9538038ec4ea26
ktnyt <kotone@sfc.keio.ac.jp>
2017-01-23 16:55:19 +0900

    Update delegate definition

--- src/delegate.rs
+++ src/delegate.rs
@@ -1,79 +1,36 @@
 #[macro_export]
 macro_rules! delegate {
-    { to $base:ident; $($tt:tt)* } =>
-    { delegate! { @expand_fn $base $($tt)* } };
+    { to $base:ident; $($tt:tt)* } => { delegate! { @expand $base $($tt)* } };
 
-    { @expand_fn $base:ident fn $func:ident(self); $($tt:tt)* } =>
-    { fn $func(self) { self.$base.$func(); } delegate! { @expand_fn $base $($tt)* } };
+    { @expand $base:ident fn $func:ident(self $(, $name:ident : $ty:ty)*) -> $ret:ty; $($rest:tt)* } => {
+        fn $func(self $(, $name:$ty)*) -> $ret { self.$base.$func($($name),*) }
+        delegate! { @expand $base $($rest)* }
+    };
 
-    { @expand_fn $base:ident fn $func:ident(&self); $($tt:tt)* } =>
-    { fn $func(&self) { self.$base.$func(); } delegate! { @expand_fn $base $($tt)* } };
+    { @expand $base:ident fn $func:ident(self $(, $name:ident : $ty:ty)*); $($rest:tt)* } => {
+        fn $func(self $(, $name:$ty)*) { self.$base.$func($($name),*); }
+        delegate! { @expand $base $($rest)* }
+    };
 
-    { @expand_fn $base:ident fn $func:ident(&mut self); $($tt:tt)* } =>
-    { fn $func(&mut self) { self.$base.$func(); } delegate! { @expand_fn $base $($tt)* } };
+    { @expand $base:ident fn $func:ident(&self $(, $name:ident : $ty:ty)*) -> $ret:ty; $($rest:tt)* } => {
+        fn $func(&self $(, $name:$ty)*) -> $ret { self.$base.$func($($name),*) }
+        delegate! { @expand $base $($rest)* }
+    };
 
-    { @expand_fn $base:ident fn $func:ident(self) -> $ret:ty; $($tt:tt)* } =>
-    { fn $func(self) -> $ret { self.$base.$func() } delegate! { @expand_fn $base $($tt)* } };
+    { @expand $base:ident fn $func:ident(&self $(, $name:ident : $ty:ty)*); $($rest:tt)* } => {
+        fn $func(&self $(, $name:$ty)*) { self.$base.$func($($name),*); }
+        delegate! { @expand $base $($rest)* }
+    };
 
-    { @expand_fn $base:ident fn $func:ident(&self) -> $ret:ty; $($tt:tt)* } =>
-    { fn $func(&self) -> $ret { self.$base.$func() } delegate! { @expand_fn $base $($tt)* } };
+    { @expand $base:ident fn $func:ident(&mut self $(, $name:ident : $ty:ty)*) -> $ret:ty; $($rest:tt)* } => {
+        fn $func(&mut self $(, $name:$ty)*) -> $ret { self.$base.$func($($name),*) }
+        delegate! { @expand $base $($rest)* }
+    };
 
-    { @expand_fn $base:ident fn $func:ident(&mut self) -> $ret:ty; $($tt:tt)* } =>
-    { fn $func(&mut self) -> $ret { self.$base.$func() } delegate! { @expand_fn $base $($tt)* } };
+    { @expand $base:ident fn $func:ident(&mut self $(, $name:ident : $ty:ty)*); $($rest:tt)* } => {
+        fn $func(&mut self $(, $name:$ty)*) { self.$base.$func($($name),*); }
+        delegate! { @expand $base $($rest)* }
+    };
 
-    { @expand_fn $base:ident fn $func:ident(self, $($name:ident : $ty:ty),*); $($tt:tt)* } =>
-    { fn $func(self, $($name:$ty),*) { self.$base.$func($($name),*); } delegate! { @expand_fn $base $($tt)* } };
-
-    { @expand_fn $base:ident fn $func:ident(&self, $($name:ident : $ty:ty),*); $($tt:tt)* } =>
-    { fn $func(&self, $($name:$ty),*) { self.$base.$func($($name),*); } delegate! { @expand_fn $base $($tt)* } };
-
-    { @expand_fn $base:ident fn $func:ident(&mut self, $($name:ident : $ty:ty),*); $($tt:tt)* } =>
-    { fn $func(&mut self, $($name:$ty),*) { self.$base.$func($($name),*); } delegate! { @expand_fn $base $($tt)* } };
-
-    { @expand_fn $base:ident fn $func:ident(self, $($name:ident : $ty:ty),*) -> $ret:ty; $($tt:tt)* } =>
-    { fn $func(self, $($name:$ty),*) -> $ret { self.$base.$func($($name),*) } delegate! { @expand_fn $base $($tt)* } };
-
-    { @expand_fn $base:ident fn $func:ident(&self, $($name:ident : $ty:ty),*) -> $ret:ty; $($tt:tt)* } =>
-    { fn $func(&self, $($name:$ty),*) -> $ret { self.$base.$func($($name),*) } delegate! { @expand_fn $base $($tt)* } };
-
-    { @expand_fn $base:ident fn $func:ident(&mut self, $($name:ident : $ty:ty),*) -> $ret:ty; $($tt:tt)* } =>
-    { fn $func(&mut self, $($name:$ty),*) -> $ret { self.$base.$func($($name),*) } delegate! { @expand_fn $base $($tt)* } };
-
-    { @expand_fn $base:ident } => {};
-
-    { @expand_fn $base:ident fn $func:ident(self); } =>
-    { fn $func(self) { self.$base.$func(); } };
-
-    { @expand_fn $base:ident fn $func:ident(&self); } =>
-    { fn $func(&self) { self.$base.$func(); } };
-
-    { @expand_fn $base:ident fn $func:ident(&mut self); } =>
-    { fn $func(&mut self) { self.$base.$func(); } };
-
-    { @expand_fn $base:ident fn $func:ident(self) -> $ret:ty; } =>
-    { fn $func(self) -> $ret { self.$base.$func() } };
-
-    { @expand_fn $base:ident fn $func:ident(&self) -> $ret:ty; } =>
-    { fn $func(&self) -> $ret { self.$base.$func() } };
-
-    { @expand_fn $base:ident fn $func:ident(&mut self) -> $ret:ty; } =>
-    { fn $func(&mut self) -> $ret { self.$base.$func() } };
-
-    { @expand_fn $base:ident fn $func:ident(self, $($name:ident : $ty:ty),*); } =>
-    { fn $func(self, $($name:$ty),*) { self.$base.$func($($name),*); } };
-
-    { @expand_fn $base:ident fn $func:ident(&self, $($name:ident : $ty:ty),*); } =>
-    { fn $func(&self, $($name:$ty),*) { self.$base.$func($($name),*); } };
-
-    { @expand_fn $base:ident fn $func:ident(&mut self, $($name:ident : $ty:ty),*); } =>
-    { fn $func(&mut self, $($name:$ty),*) { self.$base.$func($($name),*); } };
-
-    { @expand_fn $base:ident fn $func:ident(self, $($name:ident : $ty:ty),*) -> $ret:ty; } =>
-    { fn $func(self, $($name:$ty),*) -> $ret { self.$base.$func($($name),*) } };
-
-    { @expand_fn $base:ident fn $func:ident(&self, $($name:ident : $ty:ty),*) -> $ret:ty; } =>
-    { fn $func(&self, $($name:$ty),*) -> $ret { self.$base.$func($($name),*) } };
-
-    { @expand_fn $base:ident fn $func:ident(&mut self, $($name:ident : $ty:ty),*) -> $ret:ty; } =>
-    { fn $func(&mut self, $($name:$ty),*) -> $ret { self.$base.$func($($name),*) } };
+    { @expand $base:ident } => {};
 }
